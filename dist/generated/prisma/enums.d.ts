d17dc962a557264d066a8f040047340684616368
Hannahlab-pe <operaciones@hannahlab.com>
2026-06-13 09:59:21 -0500

    chore: untrack dist (se genera en build)

--- dist/generated/prisma/enums.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const OrderStatus: {
-    readonly PAID: "PAID";
-    readonly PROCESSING: "PROCESSING";
-    readonly SHIPPED: "SHIPPED";
-    readonly DELIVERED: "DELIVERED";
-    readonly CANCELLED: "CANCELLED";
-};
-export type OrderStatus = (typeof OrderStatus)[keyof typeof OrderStatus];
